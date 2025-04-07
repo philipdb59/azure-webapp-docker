37bfa7186b701192e5e91b63c4613ba14a10a383
Paul-MHP <paul.schiel@mhp.com>
2025-04-07 23:35:32 +0200

    Update app.py

--- app.py
+++ app.py
@@ -1,31 +1,11 @@
-import gradio as gr 
+from flask import Flask
 
-from transformers import pipeline 
+app = Flask(__name__)
 
- 
+@app.route("/")
+def hello_world():
+    return "Hello from a simplified Flask app!"
 
-classifier = pipeline("text-classification",model='bhadresh-savani/distilbert-base-uncased-emotion', return_all_scores=True) 
-
- 
-
-def detect_emotions(emotion_input): 
-
-  prediction = classifier(emotion_input,) 
-
-  output = {} 
-
-  for emotion in prediction[0]: 
-
-    output[emotion["label"]] = emotion["score"] 
-
-  return output 
-
- 
-
-examples = [["I am excited to announce that I have been promoted"], ["Sorry for the late reply"]] 
-
- 
-
-demo = gr.Interface(fn=detect_emotions, inputs=gr.Textbox(placeholder="Enter text here", label="Input"), outputs=gr.Label(label="Emotion"), examples=examples) 
-
-demo.launch(server_name="0.0.0.0", server_port=7000) 
+if __name__ == "__main__":
+    # Important: Bind to '0.0.0.0' and use port 7000
+    app.run(host="0.0.0.0", port=7000)
